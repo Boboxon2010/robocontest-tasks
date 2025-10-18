ea15024053ebeee6c598f47f47ff84721fdfb54a
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-10-18 12:03:51 +0500

    updated

--- robocontest1099.py
+++ robocontest1099.py
@@ -1,8 +1,23 @@
 # masala: Sizga ikkita natural son beriladi. Sizning vazifangiz shu sonlar orasidagi 3ga bo'linadigan ammo 7 bo'linmaydigan sonlar yigindisini topish. Bunda ikkala chegara ham kiradi. Time limit (test 101) xatoliki chiqmasin
 a,b=map(int,input().split())
-s=0
-for i in range(a,b+1):
-    if i%3==0 and i%7!=0:
-        s+=i    
-print(s)
+if a>b:
+    a,b = b,a
+
+def sum_of_multiples(k, L, R):
+    """Return sum of multiples of k in [L, R] using arithmetic progression."""
+    if L>R:
+        return 0
+    # first multiple of k >= L
+    first = ((L + k - 1) // k) * k
+    # last multiple of k <= R
+    last = (R // k) * k
+    if first>last:
+        return 0
+    n = (last - first) // k + 1
+    return n * (first + last) // 2
+
+# Sum multiples of 3 but exclude those divisible by 7 (i.e., divisible by lcm(3,7)=21)
+sum3 = sum_of_multiples(3, a, b)
+sum21 = sum_of_multiples(21, a, b)
+print(sum3 - sum21)
 
