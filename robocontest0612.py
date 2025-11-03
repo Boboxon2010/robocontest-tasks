1e180ba0a459a50e1b73579935656ad42cea9957
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-03 11:42:29 +0500

    updated

---
+++ robocontest0612.py
@@ -0,0 +1,9 @@
+# Kirishni o'qiyapmiz
+a, b = input().split()
+
+# Harflarni songa o'tkazamiz
+num1 = ord(a) - ord('A') + 1
+num2 = ord(b) - ord('A') + 1
+
+# Yig'indini chiqaramiz
+print(num1 + num2)
