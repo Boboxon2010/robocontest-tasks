37eab103beb701c241ce6defa13fbf93b4cc6d60
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-12 09:26:55 +0500

    updated

--- robocontest1035/tasks/AM.py
+++ robocontest1035/tasks/AM.py
@@ -1,9 +1,15 @@
-n=int(input())
-x=float(input())
-s=x
-k=1
-for i in range(1,n+1):
-    
-    s+=(2*i-1)*(pow(x,2*i+1))/(2*(2*i)*(2*i+1))
-    
-print(f"{s:.8f}")
+n = int(input())
+x = float(input())
+
+S = 0.0
+odd_fact = 1.0   # 1*3*5*...
+even_fact = 1.0  # 2*4*6*...
+
+for i in range(0, n + 1):
+    if i > 0:
+        odd_fact *= (2 * i - 1)
+        even_fact *= (2 * i)
+    term = odd_fact * x**(2 * i + 1) / (even_fact * (2 * i + 1))
+    S += term
+
+print(f"{S:.8f}")
