4c2fdb82044413e4ea371e87fb18713dee245107
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-14 11:27:02 +0500

    updated

--- olympiads 1010/tasks/S.py
+++ olympiads 1010/tasks/S.py
@@ -1,2 +1,8 @@
-# S.py
-# Avtomatik yaratilgan fayl
+
+a=int(input())
+b=int(input())
+c=int(input())
+if (a<0 and b>0 and c>0) or (a>0 and b<0 and c>0) or (a>0 and b>0 and c<0):
+    print("Rost")
+else:
+    print("yolg'on")
