76d735f9fcab0ad3e7f1a3ba49c3463ad15c66f3
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-19 11:34:50 +0500

    updated

---
+++ olimpiada1007/tasks/G.py
@@ -0,0 +1,14 @@
+n=int(input())
+list=[]
+for i in range(1,n+1):
+    list.insert(i,0)
+for i in list:
+    print(i,end=' ')
+    
+    """ yana bir usul"""
+
+# n=int(input())
+# list=list(range(1,n+1))
+# list.sort(reverse=True)
+# for i in list:
+#     print(i,end=' ')
