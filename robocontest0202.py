f8da55ad39feecbde9bf97b13309b15986f8fbfb
Boboxon <boboxonjumaboyev2010@gmail.com>
2025-11-22 09:58:02 +0500

    updated

--- robocontest0202.py
+++ robocontest0202.py
@@ -1,3 +1,27 @@
 n=int(input())
-if n==0:
-    print()
+a=str(n)
+s=0
+for i in a:
+    i=int(i)
+    if i==0:
+        s+=6
+    elif i==1:
+        s+=2
+    elif i==2:
+        s+=5
+    elif i==3:
+        s+=5
+    elif i==4:
+        s+=4
+    elif i==5:
+        s+=5
+    elif i==6:
+        s+=6
+    elif i==7:
+        s+=3
+    elif i==8:
+        s+=7
+    elif i==9:
+        s+=6
+print(s)
+    
